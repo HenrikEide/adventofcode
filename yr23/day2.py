4d8eaaf58a91c14ad914733a069a6ad3cf46abe2
HenrikEide <Henrik225@gmail.com>
2023-12-02 11:56:22 +0100

    part2

--- yr23/day2.py
+++ yr23/day2.py
@@ -1,20 +1,44 @@
 from sys import stdin
+from math import prod
 ls = [l.split(':')[1].split(';') for l in stdin.readlines()]
 ls = [[x.strip() for x in l] for l in ls]
 tot = 0
 for i,l in enumerate(ls):
-    g = 0
+    g = [0,0,0]
     for cs in l:
         for c in cs.split(','):
             n,c = c.split()
             n = int(n)
             match c:
                 case 'blue':
-                    g += n>14
+                    g[0] = max(g[0],n)
                 case 'green':
-                    g += n>13
+                    g[1] = max(g[1],n)
                 case 'red':
-                    g += n>12
-    tot += (i+1)*(not g)
+                    g[2] = max(g[2],n)
+    tot += prod(g)
 print(tot)
+
+
+# Part 1:
+
+# from sys import stdin
+# ls = [l.split(':')[1].split(';') for l in stdin.readlines()]
+# ls = [[x.strip() for x in l] for l in ls]
+# tot = 0
+# for i,l in enumerate(ls):
+#     g = 0
+#     for cs in l:
+#         for c in cs.split(','):
+#             n,c = c.split()
+#             n = int(n)
+#             match c:
+#                 case 'blue':
+#                     g += n>14
+#                 case 'green':
+#                     g += n>13
+#                 case 'red':
+#                     g += n>12
+#     tot += (i+1)*(not g)
+# print(tot)
             
