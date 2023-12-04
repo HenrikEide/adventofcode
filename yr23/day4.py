eb37cbb76e58de815b286116a8899f04e38e0f55
HenrikEide <Henrik225@gmail.com>
2023-12-04 13:59:48 +0100

    shalom

--- yr23/day4.py
+++ yr23/day4.py
@@ -2,8 +2,8 @@ import sys
 cards = [l.split(':')[1].split('|') for l in sys.stdin.readlines()]
 tot = [1]*len(cards)
 for i,card in enumerate(cards):
-    w, h = [x.split() for x in card]
-    nums = sum([x in h for x in w])
-    for j in range(i+1,i+nums+1):
+    w,h = [x.split() for x in card]
+    score = sum([n in h for n in w])
+    for j in range(i+1, i+score+1):
         tot[j] += tot[i]
 print(sum(tot))
