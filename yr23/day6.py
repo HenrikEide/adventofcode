674077a0daef4069e393c4e33bab962771ed2888
HenrikEide <Henrik225@gmail.com>
2023-12-06 11:43:21 +0100

    lil optimus

--- yr23/day6.py
+++ yr23/day6.py
@@ -1,17 +1,12 @@
-rstest = [(7,9), (15,40), (30,200)]
 rs1 = [(40,215), (92,1064), (97,1505), (90,1100)]
 rs2 = [(40929790,215106415051100)]
 ans = 1
 for t,d in rs2:
-    start, end = 0,t
+    speed = 0
     while 1:
-        start += 1
-        if start*(t-start) > d:
+        speed += 1
+        if speed*(t-speed) > d:
             break
-    while 1:
-        end -= 1
-        if end*(t-end) > d:
-            break
-    ans *= end-start+1
-    
+    ans *= t-speed*2+1
+
 print(ans)
