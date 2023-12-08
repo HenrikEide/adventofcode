562c8226fa449fc0409bd1e3d3365b3eb80553f1
HenrikEide <Henrik225@gmail.com>
2023-12-08 11:58:30 +0100

    lil cleanup

--- yr23/day8.py
+++ yr23/day8.py
@@ -1,16 +1,24 @@
 import sys
+from math import lcm
 lr, m = sys.stdin.read().split('\n\n')
 m = [l.split(" = ") for l in m.split('\n')]
-d  = {x[0]:x[1][1:-1].split(', ') for x in m}
-curr = [x for x in d.keys() if x[2]=='A']
-print(curr)
+locMap  = {x[0]:x[1][1:-1].split(', ') for x in m}
+
+curr = [x for x in locMap.keys() if x[2]=='A']
+dists = []
 i = 0
-ans = 0
-while not all([x[2]=='Z' for x in curr]):
+ans = 1
+while 1:
     match lr[i]:
-        case 'L': curr = [d[x][0] for x in curr]
-        case 'R': curr = [d[x][1] for x in curr]
+        case 'L': curr = [locMap[x][0] for x in curr]
+        case 'R': curr = [locMap[x][1] for x in curr]
+    for x in curr:
+        if x[2]=='Z':
+            dists.append(ans)
+            curr.remove(x)
+    if not curr:
+        break
     i = (i+1)%len(lr)
     ans += 1
 
-print(ans)
+print(lcm(*dists))
