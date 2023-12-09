3faccc2846ab34a3341c2e1dd5665c0f0df0bfc2
HenrikEide <Henrik225@gmail.com>
2023-12-09 13:10:04 +0100

    part2 was tiny change

--- yr23/day9.py
+++ yr23/day9.py
@@ -9,8 +9,7 @@ for his in ns:
         dif.append([0]*~-len(dif[-1]))
         for i in range(len(dif[-2])-1):
             dif[-1][i] = dif[-2][i+1] - dif[-2][i]
-    dif = dif[::-1]
-    dif = [x[::-1] for x in dif]
+    dif = [x[::-1] for x in dif][::-1]
     for i in range(len(dif)-1):
         dif[i+1].append(dif[i+1][-1]-dif[i][-1])
     ans += dif[-1][-1]
