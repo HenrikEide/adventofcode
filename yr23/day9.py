b46236a25091109082c29197fbe6656266b41229
HenrikEide <Henrik225@gmail.com>
2023-12-09 09:17:35 +0100

    part1

---
+++ yr23/day9.py
@@ -0,0 +1,15 @@
+import sys
+ns = [[int(x) for x in line.split()] for line in sys.stdin.readlines()]
+
+ans = 0
+for his in ns:
+    dif = [his]
+    while any(dif[-1]):
+        dif.append([0]*~-len(dif[-1]))
+        for i in range(len(dif[-2])-1):
+            dif[-1][i] = dif[-2][i+1] - dif[-2][i]
+    dif = dif[::-1]
+    for i in range(len(dif)-1):
+        dif[i+1].append(dif[i+1][-1]+dif[i][-1])
+    ans += dif[-1][-1]
+print(ans)
