17efb472e8c0600ec28f57e002810342d1c64822
HenrikEide <Henrik225@gmail.com>
2023-12-15 09:57:09 +0100

    blessed

--- yr23/day15.py
+++ yr23/day15.py
@@ -21,21 +21,11 @@ for s in seq:
 ans = 0
 for i,box in enumerate(boxs):
     for j,l in enumerate(box.items()):
-        ans += (i+1)*(j+1)*int(l[1])
+        ans += (i+1)*(j+1)*l[1]
 print(ans)
 
 # Part 1:
-# seq = input().split(',')
-
-# def hash(s:str) -> int:
-#     curr = 0
-#     for c in s:
-#         curr += ord(c)
-#         curr *= 17
-#         curr %= 256
-#     return curr
-
-# ans = 0
-# for s in seq:
-#     print(hash(s[:2]))
-# print(ans)
+ans = 0
+for s in seq:
+    print(hash(s[:2]))
+print(ans)
