0a684bfea08919e5211ac5a735f609e73e7280de
HenrikEide <Henrik225@gmail.com>
2024-12-07 14:46:07 +0100

    parto

--- yr24/day5.hs
+++ yr24/day5.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad (ap)
-import Data.Map (Map, empty, findWithDefault, fromList, insertWith, keys)
+import Data.Map (Map, findWithDefault, fromList)
 import Data.Maybe (fromMaybe)
 import Data.Text (lines, pack, splitOn, unpack)
 
 main = do
   [xs, ys] <- fmap (map Data.Text.lines . splitOn "\n\n" . pack) getContents
   let rules = map (map (read . unpack) . reverse . splitOn "|") xs
+  let pages = map (map (read . unpack) . splitOn ",") ys
   let rulesM = fromList $ map ((\k -> (k, [head (tail x) | x <- rules, head x == k])) . head) rules :: Map Int [Int]
-  print $ sum $ map (ap (!!) ((`div` 2) . length)) (filter (checkOrdering rulesM) (map (map (read . unpack) . splitOn ",") ys))
+  print $ sum $ map (ap (!!) ((`div` 2) . length)) (filter (checkOrdering rulesM) pages)
+  print $ sum $ map (ap (!!) ((`div` 2) . length) . (!! 100) . iterate (sortPages rulesM)) (filter (not . checkOrdering rulesM) pages)
 
 checkOrdering :: Map Int [Int] -> [Int] -> Bool
 checkOrdering rulesM (x : xs)
   | null xs = True
   | any (`elem` xs) (findWithDefault [] x rulesM) = False
   | otherwise = checkOrdering rulesM xs
+
+sortPages :: Map Int [Int] -> [Int] -> [Int]
+sortPages rulesM (x : xs)
+  | null xs = [x]
+  | any (`elem` xs) (findWithDefault [] x rulesM) = sortPages rulesM xs ++ [x]
+  | otherwise = x : sortPages rulesM xs
