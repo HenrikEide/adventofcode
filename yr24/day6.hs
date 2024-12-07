704e48e9f1ccbf73ee2c46cb2f5fd62f0b8908b5
HenrikEide <Henrik225@gmail.com>
2024-12-07 21:16:30 +0100

    oops all logical errors

--- yr24/day6.hs
+++ yr24/day6.hs
@@ -4,13 +4,28 @@ main = do
   grid <- lines <$> getContents
   let test = (4, 6)
   let input = (95, 67)
-  print $ length $ nub $ part1 grid [] input North
+  --   print $ length $ nub $ part1 grid [] input North
+  let visited = part2 grid [] input North
+  --   print visited
+  print $ filter (checkForLoop grid visited) visited
 
 part1 :: [String] -> [Pos] -> Pos -> Dir -> [Pos]
 part1 grid visited pos dir = case get grid (move pos dir) of
   '.' -> part1 grid (pos : visited) (move pos dir) dir
   '#' -> part1 grid (pos : visited) (move pos (turn dir)) (turn dir)
   _ -> pos : visited
+
+part2 :: [String] -> [(Pos, Dir)] -> Pos -> Dir -> [(Pos, Dir)]
+part2 grid visited pos dir = case get grid (move pos dir) of
+  '.' -> part2 grid ((pos, dir) : visited) (move pos dir) dir
+  '#' -> part2 grid ((pos, turn dir) : visited) (move pos (turn dir)) (turn dir)
+  _ -> (pos, dir) : visited
+
+checkForLoop :: [String] -> [(Pos, Dir)] -> (Pos, Dir) -> Bool
+checkForLoop grid xs (pos, dir) = any (\p -> p `elem` goLong grid (move pos (turn dir), turn dir)) xs
+
+goLong :: [String] -> (Pos, Dir) -> [(Pos, Dir)]
+goLong grid (pos, dir) = takeWhile (\(x, y) -> get grid x == '.') $ iterate (\(p, d) -> (move p d, d)) (pos, dir)
 
 type Pos = (Int, Int)
 
