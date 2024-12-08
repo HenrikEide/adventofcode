f3a1430c707eddaceaf905d85b029bc58c83d173
HenrikEide <Henrik225@gmail.com>
2024-12-08 16:07:21 +0100

    parto

--- yr24/day7.hs
+++ yr24/day7.hs
@@ -6,8 +6,15 @@ import Data.Text (pack, splitOn, unpack)
 main = do
   xs <- map pack . lines <$> getContents
   let eqs = map ((\(x : y : _) -> (read x, map read (words y))) . map unpack . splitOn ": ") xs
-  print $ sum $ map fst $ filter (\(x, y) -> x `elem` genEqs (tail y) [head y]) eqs
+  print $ tot eqs genEqs
+  print $ tot eqs part2
+
+tot xs g = sum $ map fst $ filter (\(x, y) -> x `elem` g (tail y) [head y]) xs
 
 genEqs :: [Int] -> [Int] -> [Int]
 genEqs [x] eqs = map (+ x) eqs ++ map (* x) eqs
 genEqs (x : xs) eqs = genEqs xs $ map (+ x) eqs ++ map (* x) eqs
+
+part2 :: [Int] -> [Int] -> [Int]
+part2 [x] eqs = map (+ x) eqs ++ map (* x) eqs ++ map (read . (++ show x) . show) eqs
+part2 (x : xs) eqs = part2 xs $ map (+ x) eqs ++ map (* x) eqs ++ map (read . (++ show x) . show) eqs
